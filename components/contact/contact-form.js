0d0dd28b25bd58849176149cf8f91a6b44e2f318
Adelino Justo <adelinojusto911@gmail.com>
2025-01-15 20:21:10 +0800

    Change Notification message

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -58,7 +58,7 @@ function ContactForm(){
         notification = {
             status:'success',
             title:'Success!',
-            message:'Message sent successfully!'
+            message:'Message sent successfully!!!'
         }
     else if(requestStatus ==='error')
         notification = {
